e19f472b71fb5b03e4707e22abd52bd6a5c059b7
lonlon11 <11627627+lonlon11@users.noreply.github.com>
2021-03-16 13:18:39 +0100

    renamed python file

--- Tasks/T_1/T_1.py
+++ Tasks/T_1/T_1.py
@@ -8,13 +8,15 @@ import numpy as np
 
 # importing the data from the csv file
 file = np.genfromtxt('train.csv', delimiter=',')
+
+# file[x:y, s:t] -> in rows x to y take elements s to t
 y = file[1:, 0]      # Ignoring the header line, the first column is the y array
 X = file[1:, 1:]     # Ignoring the header line, the rest is the X array
 
 
 # define the cross-validation model
 # TODO: understand the parameters and make sure they are right
-cv = model_selection.KFold(n_splits=10, random_state=1, shuffle=True)
+cv = model_selection.KFold(n_splits=10)
 
 
 # lambdas array
@@ -25,36 +27,25 @@ lmb = np.array([0.1, 1, 10, 100, 200])
 res = np.zeros(len(lmb))
 
 
-# calculating the RSME
-def rsme(x):
-    return np.sqrt(np.mean(np.absolute(x)))
-
-
-# calculating the hyper parameters for any lambda lm
-def hyperpara(lm):
+# calculating the RSME for every hyper parameter lm
+def rsme(lm):
     # define the regression model
     model = linear_model.Ridge(alpha=lm)
 
     # TODO: understand the parameters and make sure they are right!
     # use k-fold CV to evaluate model
-    scores = model_selection.cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv, n_jobs=-1)
+    scores = model_selection.cross_val_score(model, X, y, cv=cv)
 
-    # print(rsme(scores))
-    return rsme(scores)
+    # print(scores)
+    # TODO: are we calculating the RSME correctly?
+    return np.sqrt(np.mean(np.absolute(scores)))
 
 
 # calculate the hyper parameters
 for i in range(len(lmb)):
-    res[i] = hyperpara(lmb[i])
+    res[i] = rsme(lmb[i])
 
 
 # export the results in the csv file
-# TODO: the output file name needs to be changed to the correct 'sample.csv'
+# TODO: rename the output file
 np.savetxt('res.csv', res, delimiter=',')
-
-# this code is obsolete
-# with open('res.csv', 'w') as output:
-#     writer = csv.writer(output)
-#     writer.writerow(res)
-# output.close()
-
